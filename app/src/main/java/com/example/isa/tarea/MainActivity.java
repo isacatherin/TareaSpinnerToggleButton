3c451fb312201435d8136b445a491d31ba89d7b3
Isa <isacatherin08@gmail.com>
2017-05-06 11:15:07 -0500

    Tarea terminada

--- app/src/main/java/com/example/isa/tarea/MainActivity.java
+++ app/src/main/java/com/example/isa/tarea/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
 
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void OnCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
